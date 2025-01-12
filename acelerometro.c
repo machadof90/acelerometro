d1949931bd7dfa7eb991c9b4cc7d028872753e88
machadof90 <valterig@hotmail.com>
2025-01-12 02:02:07 -0300

    corrige sintaxe dos casos no controle de cores e adiciona geração de cor aleatória

--- acelerometro.c
+++ acelerometro.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "include/pinosCores.c" 
-#include <time.h>  
+#include "include/pinosCores.c"
+#include <time.h>
 
 #define LED_R 13
 #define LED_B 12
@@ -14,57 +14,63 @@
 
 // inicia componentes
 int tempo = 50;
-void cor(int num){
+void cor(int num)
+{
     switch (num)
     {
-    case: 1
+    case 1:
         vermelho();
         break;
-    
-    case: 2
+
+    case 2:
         azul();
         break;
-    
-    case: 3
+
+    case 3:
         amarelo();
         break;
-    
-    case: 4
+
+    case 4:
         rosa();
         break;
-    
-    case: 5
+
+    case 5:
         ciano();
-        break;    
-        
-    case: 6 
-        branco();    
-        break;    
-        
-    case: 7
+        break;
+
+    case 6:
+        branco();
+        break;
+
+    case 7:
         verde();
-        break;    
-    default:
         break;
     }
 }
-
 
 int main()
 {
     stdio_init_all();
     iniciaComponentes();
     while (true)
-    {           
-        vermelho();
+    {
+        srand(time(NULL));
+        int numAleatorio = (rand() % 7) + 1;
+        cor(numAleatorio);
         sleep_ms(tempo);
         apagar();
         sleep_ms(tempo);
-        if(gpio_get(BUTTON_A) == 0 && tempo > 50){
-            tempo = tempo - 50;            
-        }else if(gpio_get(BUTTON_B) == 0 && tempo < 1000){
+        if (gpio_get(BUTTON_A) == 0 && tempo > 50)
+        {
+            tempo = tempo - 50;
+        }
+        else if (gpio_get(BUTTON_B) == 0 && tempo < 1000)
+        {
             tempo = tempo + 50;
-        }    
-        if(gpio_get(BUTTON_J) == 0){    
+        }
+        if (gpio_get(BUTTON_J) == 0)
+        {
+            cor(numAleatorio);
+        }
     }
 }
